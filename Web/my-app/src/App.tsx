5d358f7e6ee6ea662de8d7621fde914110757b22
suparnadey-5 <miss.s.dey05@gmail.com>
2025-02-01 19:35:47 +0530

    header added

--- Web/my-app/src/App.tsx
+++ Web/my-app/src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import { Header } from './components/Header'
 import { Home } from './components/Home'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         My Online Coaching
       </h1>
       <Home/>
+      <Header/>
       
     </>
   )
