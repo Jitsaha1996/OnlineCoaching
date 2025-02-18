4d0fa6fc1f9ef0f72314939da878c928c6ecb271
debraj-06 <rajdeb2522a@gmail.com>
2025-02-18 21:41:13 +0530

    fix visuals of navbar

--- Web/my-app/src/components/NavBar.tsx
+++ Web/my-app/src/components/NavBar.tsx
@@ -4,12 +4,12 @@ export default function Navbar() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-    <nav className="fixed top-0 left-0 w-full flex justify-between items-center p-6 bg-gradient-to-r from-gray-900 to-gray-700 text-white shadow-lg z-50">
-      <h1 className="text-xs  tracking-wide">
+    <nav className="fixed top-0 left-0 w-full flex justify-between items-center p-6 bg-gradient-to-r from-blue-900 to-blue-700 text-white shadow-lg z-50">
+      <h1 className="text-xs tracking-wide">
         {isLoggedIn ? "Welcome Debraj" : "Welcome"}
       </h1>
       <button 
-        className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-full font-semibold transition duration-300"
+        className="bg-white text-black px-6 py-3 rounded-full font-semibold transition duration-300 hover:bg-gray-200"
         onClick={() => setIsLoggedIn(!isLoggedIn)}
       >
         {isLoggedIn ? "Logout" : "Login"}
