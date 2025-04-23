5a5a1a210a40ffe5eec834a06539863faa82a315
todo 2004 <sayantandutta12122004@gmai.com>
2025-04-23 23:14:39 +0530

    db.ts added

---
+++ Backend/src/config/db.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDb = async (): Promise<void> => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL || "");
+        console.log(`MongoDB connectDb: ${conn.connection.host}`);
+    } catch (error: any) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1); // Exit with a failure code
+    }
+};
+
+export default connectDb;
